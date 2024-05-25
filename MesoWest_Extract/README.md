0e84ac4bef4f19a3442d7d3b7d5ffa9967451db6
John-C-Lin <30577430+John-C-Lin@users.noreply.github.com>
2024-05-25 13:12:20 -0600

    Add code to extract and save MesoWest met data

---
+++ MesoWest_Extract/README.md
@@ -0,0 +1,121 @@
+R code to read in and process MesoWest met data
+
+
+Below are some notes from emails discussing these MesoWest stations:
+
+On Fri, Mar 15, 2019 at 10:01 AM Alex Jacques <alexander.jacques@utah.edu> wrote:
+Just one word of caution there that the list was picked based on the general ability of being reasonably representative of conditions (i.e. no giant obstacles in the way, exposure in all directions, etc.).  That said, they still do have their nuances...
+
+1) The University of Utah ones mostly are shorter than 10m for winds, minus Flight Park South (FPS).  A few are on rooftops (WBB, NHMU, TRJO, NAA).
+2) The Utah DOT ones should be pretty good for wind (~10m tall), but they are near highways, so temperatures might be a bit warmer than expected.
+
+Just some things I figured I'd pass along, especially if related to evaluating model output.  Happy to answer any other questions as well.
+
+Alexander Jacques
+Postdoctoral Research Associate - Atmospheric Sciences/MesoWest
+University of Utah
+alexander.jacques@utah.edu
+http://home.chpc.utah.edu/~u0790486/
+
+
+On Fri, Mar 15, 2019 at 11:50 AM John C. Lin <John.Lin@utah.edu> wrote:
+Perfect--thanks!
+
+
+John
+
+
+On 3/15/19 9:15 AM, Erik Crosman wrote:
+John--
+
+This email from Alex should contain the info you need for the BAMS sites for your HRRR validation.
+
+Let us know if you have any questions.
+
+Thanks,
+
+E
+
+
+
+Logan and John,
+
+Erik forwarded along this request to John and I.  I've put together a listing of "key" MesoWest sites in the Excel spreadsheet that is attached.  I made my query a bit wider the figure window was showing, so there may be more sites than what will actually show in your figure.  Because of that, I wouldn't get deep into exact numbers here (not to mention definition of "key" site - if it comes up in reviews we could deal with at that juncture).
+
+Couple things:
+
+a) Both NAA and MTMET have the additional AQ sensors (pm and seasonal ozone) installed
+
+b) MTMET also has the CL31 ceilometer 
+
+c) MTMET also right now temporarily has the DAQ radiometer installed there, though I think that it may be going back to DAQ once their roof work is complete (Erik/John H. can you confirm?)
+
+d) The continuous sodar we operate is located at lat/lon 40.74895, -112.03392
+
+e) The DAQ sites are listed in the spreadsheet as well but I don't believe all are regulatory (Magna site just has weather sensors I think?) so I would perhaps check those with DAQ resources/contacts (their webpage perhaps).
+
+As far as text to describe the weather stations in the figure... I would suggest the following (or similar to the following to fit the text)...
+
+"Provided in this figure are key weather station locations operated by the National Weather Service, state agencies (e.g., Utah Department of Transportation and Division of Air Quality), and universities (e.g., University of Utah and Utah State University).  Additional weather stations also exist within the Salt Lake Valley and are operated by private citizens and other local organizations.  Weather observations from these platforms are retrieved and archived as part of the nationwide MesoWest data ingest and storage system (Horel et al. 2002) and distributed in real-time to government, research, the general public, and other entities via a suite of web-accessible pages and data API services.  Surface-based remote sensors within the valley are maintained by researchers within the University of Utah Atmospheric Sciences department (ceilometer and sodar) and the Utah Division of Air Quality (radiometer) that supplement the twice-daily rawinsondes launched by the National Weather Service at the Salt Lake International Airport.  A variety of web-based products are available to continuously monitor the boundary layer profiles of wind, temperature, and moisture from these remote sensors."
+
+Horel et al. 2002 ref - http://journals.ametsoc.org/doi/abs/10.1175/1520-0477%282002%29083%3C0211%3AMCMITW%3E2.3.CO%3B2
+
+I think that should cover it for the portion describing weather data assets in this paper.  Let us know if there are any other questions as well.
+
+-Alex
+
+
+
+July 9th, 2018 by John C. Lin (John.Lin@utah.edu)
+Download MesoWest meteorological data from station near HDP
+These stations would be:
+1) Alta - Mt. Baldy (AMB)
+
+To: "John C. Lin" <John.Lin@utah.edu>
+Cc: Alex Jacques <alexander.jacques@utah.edu>, "Dr. John Horel" <john.horel@utah.edu>
+
+
+Hi John,
+
+Sure, all of the fixed site PM2.5, met, and ozone data shown on the websites I've made is accessible through products powered by MesoWest.  So that opens a couple of avenues for getting the data.
+
+1)  You could get it from our MesoWest data download page at http://mesowest.utah.edu/cgi-bin/droman/download_ndb.cgi?stn=QHW.  That should give you access to both the met and air quality (PM2.5 and Ozone) data, and you could pick/choose which variables you wish to acquire.  The Hawthorne met data we get is 15-min data, while the air quality should be hourly (ozone may be 15min as well at time, actually if I remember right for Hawthorne).  To get more than a day you would need to log in/sign up for a MesoWest account.
+
+2)  Or, you can get it from our new MesoWest API services (http://mesowest.org/api/).  The API provides a bit more flexibility in terms of getting data from multiple stations at once, choosing specific time periods, etc.  It is actually the tool I use to plot fixed site data on the sites.
+
+At present, there is a CSV output option for singular station requests in the API (all other requests return in JSON format).  For example, here is Hawthorne (station ID QHW) data from 0 UTC 1 Feb to 0 UTC 10 Feb.
+
+http://api.mesowest.net/v2/stations/timeseries?&token=demotoken&start=201602010000&end=201602100000&obtimezone=utc&output=csv&stid=QHW
+
+If all you wanted was PM2.5, you could add "&vars=PM_25_concentration" to the end of that link to get this:
+
+http://api.mesowest.net/v2/stations/timeseries?&token=demotoken&start=201602010000&end=201602100000&obtimezone=utc&output=csv&stid=QHW&vars=PM_25_concentration
+
+The start and end times need to be in UTC, but you can choose "&obtimezone=local" instead of "&obtimezone=utc" to have the data returned in MDT/MST instead of UTC if desired.  Personally, I like to keep things simple when I'm doing queries and always return data in UTC, but that is just a personal preference.
+
+Hope some of this info is helpful, and let John or I know if there are any specific questions as well.
+
+-Alex
+
+Alexander Jacques
+Ph.D. Candidate - Atmospheric Sciences
+Programming Support - MesoWest
+University of Utah
+INSCC Rm. 484
+(801) 581-4362
+Alexander.Jacques@utah.edu
+
+On Tue, Feb 9, 2016 at 11:45 PM, John C. Lin <John.Lin@utah.edu> wrote:
+Hi Alex,
+
+Quick question:  just wondering where I could turn to for downloading the recent Hawthorne PM2.5 observations?
+I was hoping to do some analyses myself of PM variations in recent months.
+
+I've been following the concentrations on DAQ's website, but haven't found out a way to download the data.
+Can you tell me where to download the data?
+Or you have been grabbing the observations from the HTML file displayed on their real-time website? If so, could you point me to where on CHPC I could perhaps access the data files?
+
+Thanks,
+
+John
+
---
+++ MesoWest_Extract/readin_MesoWest.r
@@ -0,0 +1,169 @@
+#Reads in MesoWest data downloaded from API--e.g., 
+#First written on June 20th, 2017 by John C. Lin (John.Lin@utah.edu)
+
+###############
+token <- 'demotoken'
+# JCL(210321): for Uintah Basin analyses, choose whole years between 2016 and 2020 for analyses and save objects on annual timsecale to facilitate analyses later
+starts<-c("201501010000","201601010000","201701010000","201801010000","201901010000","202001010000","202101010000","202201010000","202301010000")[9]
+ends  <-c("201512312300","201612312300","201712312300","201812310000","201912312300","202012312300","202112312300","202212312300","202312312300")[9]
+
+stids <- NULL
+#Key MesoWest sites in SLV recommended by Alex Jacques
+stids <- c("FPN","FPS","HERUT","MTMET","NAA","NHMU","SUNUT","TRJO","UFD09","WBB","FARM","TPC","K36U","KSLC",
+           "KTVY","KU42","QBV","QED","QH3","QHW","QLN","QMG","QNP","QRP","QSA","BAC","CEN","KIJ","UT11","UT12",
+           "UT20","UT201","UT23","UT248","UT3","UT5","UT7","UT9","UTALP","UTBIG","UTCDF","UTCHL","UTCOL","UTDAN",
+           "UTDCD","UTDWY","UTHEB","UTJUN","UTLGP","UTLPC","UTMFS","UTORM","UTPCR","UTPKL","UTPLC","UTPR4","UTQRY",
+           "UTSTR","UTSVC","UTTPD","UTWAN","UTWLC","UCC14")[-c(1:4)]
+#stids<-stids[!stids%in%c("KSLC","KU42","QHW")]
+#Key MesoWest sites in the Uintah Basin, as recommended by Chris Foster (chris.foster9195@gmail.com)
+stids <- c(stids,c("QFL","QRS","UBCSP","UBHSP","KVEL","UBRDW"))
+stids <- c(stids,c("A1633")) # additional RedWash site (to be used as substitute for UBHSP site in 2015)
+#More MesoWest sites in the Uintah Basin to flesh out those mentioned above
+stids <- c(stids,c("BRAU1","HWAU1","CCRU1","KPRU1","NWRU1","CHPU1","DIAU1","YLSU1","KHCR","BLAU1","KVEL","K74U",
+                 "UINU1","SFLU1","RVZU1","HSRU1","FIVU1","USWU1","MCKU1"<"BRPU1","MCKU1","WNTU1","UPRU1","BCRU1",
+                 "TS654","K4VO","DRAC2","HCKC2","DMMC2","E7309","E3808","E7258"))
+#Additional MesoWest sites in northern SLC
+stids <- c(stids,c("USDR2"))
+
+avgtohrlyTF <- TRUE
+###############
+if(length(starts)!=length(ends)){stop("starts and ends have to have the same length")}
+
+for(tt in 1:length(starts)){
+
+  start<-starts[tt];end<-ends[tt]
+
+
+for(i in 1:length(stids)){
+  stid<-stids[i]
+  # problem with UFD09 in 2018, it appears
+  if(substring(start,1,4)=="2018"&stid=="UFD09"){print("Skipping UFD09 in 2018");next}
+  if(substring(start,1,4)=="2018"&stid=="WBB"){print("Skipping WBB in 2018");next}
+  if(substring(start,1,4)=="2019"&stid=="WBB"){print("Skipping WBB in 2019");next}
+  if(substring(start,1,4)=="2020"&stid=="WBB"){print("Skipping WBB in 2020");next}
+
+  resultname<-paste0(stid,"_",start,"to",end)
+  print(paste(".......... Parsing: Station",stid,"from",start,"to",end,"..........."))
+
+  strings <- try(readLines(paste0('http://api.mesowest.net/v2/stations/timeseries?',
+                              '&token=', token, '&start=', start, '&end=', end,
+                              '&stid=', stid, '&obtimezone=utc', '&output=csv')),silent=TRUE)
+  if(length(strings)<10){print("data too limited; skip");next}
+  lls<-strings[1:6]
+  #read in LAT & LON of MesoWest site
+  lats1<-as.numeric(strsplit(lls,split=" ")[[grep("LAT",lls)]])
+  LAT<-lats1[!is.na(lats1)]
+  lons1<-as.numeric(strsplit(lls,split=" ")[[grep("LON",lls)]])
+  LON<-lons1[!is.na(lons1)]
+  elev1<-as.numeric(strsplit(lls,split=" ")[[grep("ELEV",lls)]])
+  ELEVATION.ft<-elev1[!is.na(elev1)]
+  STATION<-paste(lls[grep("STATION",lls)],collapse=";")
+  print(paste(STATION,LAT,LON,ELEVATION.ft,sep="  "))
+
+
+  #Read in header comments and units lines
+  colnms<-strsplit(strings[7],split=",")[[1]]
+  colnms[colnms=="Date_Time"]<-"Time"
+  units<-strsplit(strings[8],split=",")[[1]]
+  #units<-scan(filenm,what="",comment.char="#",sep=",",nlines=1,skip=7)
+
+
+  strings <- strings[9:length(strings)]
+  xx<-strsplit(strings,split=",")
+  nn<-unlist(lapply(xx,length))
+if(length(unique(nn))==1){
+  dat <- read.table(text = strings, sep = ',', header = F, stringsAsFactors = F,col.names=colnms)
+}else{
+  if(sum(substring(colnms,1,17)%in%"weather_condition")>0){
+    print("weather_condition found!")
+    #deal with weather conditions that also have "comma" in the data--e.g., "Snow, Blowing Snow, Ice Fog"
+    nlast<-which(substring(colnms,1,17)=="weather_condition")
+    f<-function(x,nlast){
+      lastcol<-x[nlast]
+      if(is.na(lastcol))lastcol<-""
+      if(length(x)>nlast){
+        #combine last columns together 
+        lastcol<-paste(x[nlast:length(x)],collapse=",")
+      }else if(trimws(lastcol)==""){
+        lastcol<-"NA"
+      } #if(length(x)>=nlast){
+      y<-c(x[1:(nlast-1)],lastcol)
+      return(y)
+    } #f<-function(x){
+  #} #if(sum(substring(colnms,1,17)%in%"weather_condition")>0){
+  }else {
+    #if(colnms[length(colnms)]=="heat_index_set_1d"){
+    #print("heat_index_set_1d found!")
+    nlast<-length(colnms)
+    f<-function(x,nlast){
+      if(length(x)==nlast){return(x)}
+      if(length(x)<nlast){
+        lastcol<-"NA"
+      } #if(length(x)<nlast){
+      y<-c(x,lastcol);return(y)
+    } #f<-function(x){
+  } #if(colnms[length(colnms)]=="heat_index_set_1d"){
+
+  tmp<-unlist(lapply(xx,f,nlast=nlast))
+  data<-matrix(tmp,byrow=T,ncol=length(colnms))
+  dimnames(data)<-list(NULL,colnms)
+  dat<-as.data.frame(data)
+  dum<-dat
+
+  #convert the factors into numeric values
+  for(j in 1:ncol(dat)){
+    if(is.factor(dat[,j])){
+      tmp<-as.numeric(levels(dat[,j])[as.numeric(dat[,j])])
+      if(sumNA(tmp)==length(tmp))tmp<-levels(dat[,j])[as.numeric(dat[,j])]
+      dat[,j]<-tmp
+    } #if(is.factor(dat[,j])){
+  } #for(j in 1:ncol(dat)){
+
+} #if(length(unique(nn))==1){
+
+  dat$Time <- as.POSIXct(dat$Time, tz = 'UTC', format = '%Y-%m-%dT%H:%M:%SZ')
+
+
+  #convert wind vector to U- and V-components
+  if("wind_direction_set_1"%in%colnames(dat) & "wind_speed_set_1"%in%colnames(dat)){
+    print(paste("winddir & windspeed both available at:",STATION))
+    winddir<-as.numeric(dat[,"wind_direction_set_1"]) #[degrees]
+    windspd<-as.numeric(dat[,"wind_speed_set_1"])     #[m/s]
+    print(paste(range(winddir,na.rm=TRUE),collapse=" "))
+    V<-windspd*cos(pi*winddir/180)  #[m/s]
+    U<-windspd*sin(pi*winddir/180)  #[m/s]
+    Vwind<--1*V;Uwind<--1*U   #vector is direction wind is coming FROM, so need to switch signs
+    if(FALSE){ dev.new();plot(sqrt(Uwind^2+Vwind^2),windspd,pch=16) } #if(FALSE){
+    dat<-data.frame(dat,Uwind,Vwind)
+  } #if("wind_direction_set_1"%in%colnames(dat) & "wind_speed_set_1"%in%colnames(dat)){
+
+  dat$air_temp_set_1 <- as.numeric(dat$air_temp_set_1)
+  dat$wind_speed_set_1 <- as.numeric(dat$wind_speed_set_1)
+
+  DAT<-list(STATION=STATION,LAT=LAT,LON=LON,ELEVATION.ft=ELEVATION.ft,dat=dat)
+  saveRDS(DAT,file=paste0("out/",resultname,".RDS"))
+  print(paste(paste0(resultname,".RDS"),"written out"))
+  gc()
+
+if(avgtohrlyTF){
+  fdat <- DAT$dat
+  YYYYMMDDHH <- format(fdat$Time,tz='UTC',format='%Y%m%d%H')
+  Uave <- tapply(fdat[,"Uwind"],YYYYMMDDHH,mean,na.rm=TRUE)
+  Vave <- tapply(fdat[,"Vwind"],YYYYMMDDHH,mean,na.rm=TRUE)
+  WINDave <- tapply(as.numeric(fdat[,"wind_speed_set_1"]),YYYYMMDDHH,mean,na.rm=TRUE)
+  Tave <- tapply(as.numeric(fdat[,"air_temp_set_1"]),YYYYMMDDHH,mean,na.rm=TRUE)
+  Time.hrly <- strptime(names(Uave),tz='UTC',format='%Y%m%d%H')
+  Station_ID <- rep(unique(fdat[,"Station_ID"]),length(Time.hrly))
+  dat <- data.frame(Station_ID,Time=Time.hrly,Uwind=Uave,Vwind=Vave,Windspd=WINDave,Tair=Tave)
+  if(FALSE){ X11();plot(sqrt(Uave^2+Vave^2),WINDave,pch=16) } #if(FALSE){
+  DAT <- list(STATION=STATION,LAT=LAT,LON=LON,ELEVATION.ft=ELEVATION.ft,dat=dat)
+  saveRDS(DAT,file=paste0("out/",resultname,"_hrly.RDS"))
+  print(paste(paste0(resultname,"_hrly.RDS"),"written out"))
+  gc() 
+} #if(avgtohrlyTF){
+} #for(i in 1:length(stids)){
+
+
+} #for(tt in 1:length(starts)){
+
+
